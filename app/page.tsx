5dd9333f29470abee08cab5120caeeec29a8fa96
Maafa2027 <maafa.alhussain@gmail.com>
2026-04-29 13:52:38 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: 40 }}>
       <h1>🔥 موقعك شغال</h1>
-      <p>تم النشر على Vercel بنجاح</p>
+      <p>تم النشر بنجاح</p>
     </main>
   );
 }
