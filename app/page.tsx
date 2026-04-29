ade21b545a2e00193d03af8b23c5eb54ee07b447
Maafa2027 <maafa.alhussain@gmail.com>
2026-04-29 14:09:43 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: 40 }}>
-      <h1>🔥 موقعك شغال</h1>
+      <h1>🔥 موقعك شغال!!</h1>
       <p>تم النشر بنجاح</p>
     </main>
   );
